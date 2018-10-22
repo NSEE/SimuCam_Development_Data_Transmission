e5321031323f2bdd4f16755d0df42723cabbb59e
Yuri Bunduki <yuribunduki@gmail.com>
2018-10-22 18:48:38 -0300

    Criacao e inicializacao da task de controle de sub_unit

---
+++ G3U_HW_V02_2GB/Qsys_Project/software/G3U_Eth_Basic/sub_unit_control.c
@@ -0,0 +1,27 @@
+/*
+ * sub_unit_control.c
+ *
+ *  Created on: Oct 22, 2018
+ *      Author: Yuri Bunduki
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "util.h"
+
+/* MicroC/OS-II definitions */
+#include "includes.h"
+
+
+
+
+void sub_unit_control_task (){
+	INT8U error_code;		/*uCOS error code*/
+	INT8U exec_error;	/*Internal error code for the command module*/
+
+	struct _sub_config config;
+
+}
---
+++ G3U_HW_V02_2GB/Qsys_Project/software/G3U_Eth_Basic/sub_unit_control.h
@@ -0,0 +1,15 @@
+/*
+ * sub_unit_control.h
+ *
+ *  Created on: Oct 22, 2018
+ *      Author: Yuri Bunduki
+ */
+
+#ifndef SUB_UNIT_CONTROL_H_
+#define SUB_UNIT_CONTROL_H_
+
+
+
+
+
+#endif /* SUB_UNIT_CONTROL_H_ */
