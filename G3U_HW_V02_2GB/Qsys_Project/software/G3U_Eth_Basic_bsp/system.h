d114f860da7ca08a8e7099b7270b5c3f2c79de6e
Yuri Bunduki <yuribunduki@gmail.com>
2019-02-04 16:11:23 +0100

    Fixed IP ADDR

--- G3U_HW_V02_2GB/Qsys_Project/software/G3U_Eth_Basic_bsp/system.h
+++ G3U_HW_V02_2GB/Qsys_Project/software/G3U_Eth_Basic_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2_0' in SOPC Builder design 'MebX_Qsys_Project'
  * SOPC Builder design path: ../../MebX_Qsys_Project.sopcinfo
  *
- * Generated: Mon Jan 14 10:31:09 CET 2019
+ * Generated: Mon Feb 04 16:06:22 CET 2019
  */
 
 /*
@@ -336,7 +336,6 @@
  *
  */
 
-#define DHCP_CLIENT
 #define INCLUDE_TCP
 #define INICHE_DEFAULT_IF "NOT_USED"
 #define IP_FRAGMENTS
