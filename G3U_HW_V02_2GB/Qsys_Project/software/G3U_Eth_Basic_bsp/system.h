a732dee9e192af9fc283180a0aa8f33a57c29928
Yuri Bunduki <yuribunduki@gmail.com>
2019-04-26 18:51:41 -0300

    4channels optimization O1 (not sent for testing)

--- G3U_HW_V02_2GB/Qsys_Project/software/G3U_Eth_Basic_bsp/system.h
+++ G3U_HW_V02_2GB/Qsys_Project/software/G3U_Eth_Basic_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2_0' in SOPC Builder design 'MebX_Qsys_Project'
  * SOPC Builder design path: ../../MebX_Qsys_Project.sopcinfo
  *
- * Generated: Fri Apr 26 18:42:22 BRT 2019
+ * Generated: Fri Apr 26 18:47:51 BRT 2019
  */
 
 /*
