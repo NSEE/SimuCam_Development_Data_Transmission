5d76aa7cac1cca936f24f7dc051b5390e5b08acb
Yuri Bunduki <yuribunduki@gmail.com>
2019-11-28 12:07:28 -0300

    Add uart sender task to tree

---
+++ G3U_HW_V02_2GB/Qsys_Project/software/G3U_Eth_Basic/rtos/uart_receiver_task.c
@@ -0,0 +1,19 @@
+/*
+ ************************************************************************************************
+ *                                              NSEE
+ *                                             Address
+ *
+ *                                       All Rights Reserved
+ *
+ *
+ * Filename     : uart_receiver_task.c
+ * Programmer(s): Yuri Bunduki
+ * Created on: Nov 28, 2019
+ * Description  : Source file for the uart communication receiver task.
+ ************************************************************************************************
+ */
+/*$PAGE*/
+
+#include "uart_receiver_task.h"
+
+
---
+++ G3U_HW_V02_2GB/Qsys_Project/software/G3U_Eth_Basic/rtos/uart_receiver_task.h
@@ -0,0 +1,24 @@
+/*
+ ************************************************************************************************
+ *                                              NSEE
+ *                                             Address
+ *
+ *                                       All Rights Reserved
+ *
+ *
+ * Filename     : uart_receiver_task.h
+ * Programmer(s): Yuri Bunduki
+ * Created on: Nov 28, 2019
+ * Description  : Header file for the uart communication receiver task.
+ ************************************************************************************************
+ */
+/*$PAGE*/
+
+#ifndef RTOS_UART_RECEIVER_TASK_H_
+#define RTOS_UART_RECEIVER_TASK_H_
+
+
+
+
+
+#endif /* RTOS_UART_RECEIVER_TASK_H_ */
